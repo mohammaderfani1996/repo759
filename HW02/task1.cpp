a0d081cbd4a7d9ef589f4625b64c35bcbfe08233
mohammaderfani1996 <109330550+mohammaderfani1996@users.noreply.github.com>
2025-02-17 21:41:20 -0600

    Update task1.cpp

--- HW02/task1.cpp
+++ HW02/task1.cpp
@@ -13,7 +13,7 @@ using std::chrono::duration;
 
 int main(int argc, char *argv[]){
     if (argc != 2){
-        cerr << "There is an error with an argument" << std::endl;
+        cerr << "There is an error with an argument" << endl;
         return 1;
     }
     std::size_t n=std::atoi(argv[1]);
@@ -24,8 +24,8 @@ int main(int argc, char *argv[]){
     high_resolution_clock::time_point end;
     duration<double, milli> duration_sec;
 
-    float* arr=(float*)malloc(n*sizeof(double));
-    float* output=(float*)malloc(n*sizeof(double));
+    float* arr=(float*)malloc(n*sizeof(float));
+    float* output=(float*)malloc(n*sizeof(float));
     for (std::size_t i=0; i<n; i++){
         arr[i]=(-1)+static_cast<float> (rand()) / (static_cast <float> (RAND_MAX/(1-(-1))));
     }
