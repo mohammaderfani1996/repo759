2cfb4ccbe43ad4ef5a993cd709e57be196052ae6
mohammaderfani1996 <109330550+mohammaderfani1996@users.noreply.github.com>
2025-02-17 22:31:01 -0600

    Update matmul.cpp

--- HW02/matmul.cpp
+++ HW02/matmul.cpp
@@ -11,10 +11,10 @@
 using namespace std;
 
 void mmul1(const double* A, const double* B, double* C, const unsigned int n){
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
+    for (unsigned int i=0; i<n; i++){
+        for (unsigned int j=0; j<n; j++){
             C[i*n+j]=0;
-            for(int k=0; k<n;k++){
+            for(unsigned int k=0; k<n;k++){
                 C[i*n+j]=C[i*n+j]+A[i*n+k]*B[k*n+j];
             }
             
@@ -24,9 +24,9 @@ void mmul1(const double* A, const double* B, double* C, const unsigned int n){
 
 
 void mmul2(const double* A, const double* B, double* C, const unsigned int n){
-    for (int i=0; i<n;i++){
-        for (int k=0; k<n; k++){ 
-            for (int j=0; j<n; j++){
+    for (unsigned int i=0; i<n;i++){
+        for (unsigned int k=0; k<n; k++){ 
+            for (unsigned int j=0; j<n; j++){
                 C[i*n+j]=A[i*n+k]*B[k*n+j]+C[i*n+j];
             }
         }
@@ -35,9 +35,9 @@ void mmul2(const double* A, const double* B, double* C, const unsigned int n){
 
 
 void mmul3(const double* A, const double* B, double* C, const unsigned int n){
-    for (int j=0; j<n; j++){
-        for (int k=0; k<n; k++){
-            for(int i=0; i<n;i++){
+    for (unsigned int j=0; j<n; j++){
+        for (unsigned int k=0; k<n; k++){
+            for(unsigned int i=0; i<n;i++){
                 C[i*n+j]=C[i*n+j]+A[i*n+k]*B[k*n+j];
             }
             
@@ -46,10 +46,10 @@ void mmul3(const double* A, const double* B, double* C, const unsigned int n){
 }
 
 void mmul4(const std::vector<double>& A, const std::vector<double>& B, double* C, const unsigned int n){
-      for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
+      for (unsigned int i=0; i<n; i++){
+        for (unsigned int j=0; j<n; j++){
             C[i*n+j]=0;
-            for(int k=0; k<n;k++){
+            for(unsigned int k=0; k<n;k++){
                 C[i*n+j]=C[i*n+j]+(A[i*n+k])*(B[k*n+j]);
             }
             
