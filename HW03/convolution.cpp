5e8523e27474cf62f5a9e8cf0f0c645c37641cbc
mohammaderfani1996 <109330550+mohammaderfani1996@users.noreply.github.com>
2025-03-03 23:48:54 -0600

    Update convolution.cpp

--- HW03/convolution.cpp
+++ HW03/convolution.cpp
@@ -15,7 +15,7 @@ using namespace std;
 void convolve(const float *image, float *output, std::size_t n, const float *mask, std::size_t m){
     std::size_t row_index;
     std::size_t col_index;
-    #pragma omp parallel for collapse(2)
+    #pragma omp parallel for
     for(std::size_t x=0; x<n; x++){
         for (std::size_t y=0; y<n; y++){
             float sum=0;
@@ -24,15 +24,15 @@ void convolve(const float *image, float *output, std::size_t n, const float *mas
                     row_index=x+i-(m-1)/2;
                     col_index=y+j-(m-1)/2;
                     if((row_index>=n || row_index<0) || (col_index>=n || col_index<0)){
-                        #pragma omp atomic
+                        //#pragma omp atomic
                         sum=sum+mask[i*m+j]*1;
                     }
                     else if((row_index>=n || row_index<0) && (col_index>=n || col_index<0)){
-                        #pragma omp atomic
+                        //#pragma omp atomic
                         sum=sum+mask[i*m+j]*0;
                     }
                    else{
-                    #pragma omp atomic
+                    //#pragma omp atomic
                     sum=sum + mask[i*m+j]*image[row_index*n+col_index];
                    }
                 }
