492e9a99cdb4f2747f53bd03d10647e9999f1251
mohammaderfani1996 <109330550+mohammaderfani1996@users.noreply.github.com>
2025-04-24 22:45:06 -0500

    Update GPUSortBasedCollisionDetector.cuh

--- FinalProject/GPUSortBasedCollisionDetector.cuh
+++ FinalProject/GPUSortBasedCollisionDetector.cuh
@@ -12,7 +12,7 @@ class GPUSortBasedCollisionDetector : public CollisionDetector {
         GPUSortBasedCollisionDetector() {}
         ~GPUSortBasedCollisionDetector() {}
         GPUSortBasedCollisionDetector(size_t N);
-        void getLikelyCollisions(SphericalSatellite sats[], int nSats, SphericalSatellite possibleColliders[], int nPossibleColliders, double t, double tolerance, std::vector<LikelyCollision> &collisions) override;
+        void getLikelyCollisions(SphericalSatellite sats[], int nSats, SphericalSatellite possibleColliders[], int nPossibleColliders, double t, int num_threads, double tolerance, std::vector<LikelyCollision> &collisions) override;
 };
 
 
